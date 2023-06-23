d5b80451d9974ad9fccfc9ecdfb63e8185994d93
spas <spas2004dn@gmail.com>
2023-06-23 10:50:00 +0200

    organizamos codigo

--- opencv/imagenes.py
+++ opencv/imagenes.py
@@ -15,19 +15,6 @@ imghsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
 
 low_intenaity = np.array([140, 50, 50], np.uint8)
 high_intenaity = np.array([170, 255, 255], np.uint8)
-mask2 = cv2.inRange(imghsv, low_intenaity, high_intenaity)
-
-contour,_ = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-for i in contour:
-    area = cv2.contourArea(i)
-    if area > 100:
-        x, y, w, h = cv2.boundingRect(i)
-        cv2.rectangle(img1, (x, y), (x + w, y + h), (255, 0, 213), 2) # type: ignore
-
-#cv2.imshow('Image RGB',img1)
-#cv2.imshow('Image Gray', img2)
-#cv2.imshow('Image rosa', mask2)
 
 
 dir = 'C:/Users/spas2/Desktop/projects/programacion/simulador_motor/fotos'
@@ -59,8 +46,8 @@ for c in contenido:
         shutil.move(f'fotos/{c}','fotos2')
     
     
-    cv2.imshow('Image RGB',image)
-    cv2.imshow('Image RGB',mask)
+    cv2.imshow('Image Input',image)
+    cv2.imshow('Image Output',mask)
     cv2.waitKey(0)
     
 """         resultados.append({
